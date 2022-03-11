c45bd6b7477517747296ab05c9e13ba1466c0da8
Tuba Nur <yasatubanur@gmail.com>
2022-03-11 16:03:23 +0300

    Initial Commit

---
+++ 7LittleWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7LittleWords
+//
+//  Created by Tuba Nur YAŞA on 11.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
