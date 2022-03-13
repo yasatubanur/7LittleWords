ebc033596a42727dbbc2768a5014525e4202acce
Tuba Nur <yasatubanur@gmail.com>
2022-03-13 21:41:07 +0300

    levelUp function was coded

--- 7LittleWords/ViewController.swift
+++ 7LittleWords/ViewController.swift
@@ -166,7 +166,14 @@ class ViewController: UIViewController {
     }
     
     func levelUp(action : UIAlertAction){
+        level += 1
+        solutions.removeAll(keepingCapacity: true)
         
+        loadLevel()
+        
+        for button in letterButtons {
+            button.isHidden = false
+        }
     }
 
     @objc func clearTapped(_ sender : UIButton) {
